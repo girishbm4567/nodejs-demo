26099af0633f70501a863a2e551ecaecd0bfcf76
root <root@DESKTOP-5MGPHDE>
2023-03-06 20:23:05 +0530

    tt

---
+++ build/precache-manifest.8c0f1b7f66954ad13613e703a1aa515b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a853d88a218e9d3ebccfee30fbc7b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ff3208074254b65c4ff",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c0cb60e6e64e82991594",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ff3208074254b65c4ff",
+    "url": "/static/js/2.c71cdbdb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c71cdbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cb60e6e64e82991594",
+    "url": "/static/js/main.204ff19f.chunk.js"
+  },
+  {
+    "revision": "205dba4f9959a084aa7c",
+    "url": "/static/js/runtime-main.c2a4c5c8.js"
+  }
+]);
